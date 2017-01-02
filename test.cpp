b3ff1801b1066a1e52aabeedd7f15c7a435cc808
tacigar <ig.lhound@gmail.com>
2017-01-02 17:28:28 +0900

    Update test.cpp for binarysearch

--- test.cpp
+++ test.cpp
@@ -1,8 +1,10 @@
+#include <cassert>
 #include <iostream>
 #include <list>
 #include <vector>
 #include <algorithm/quicksort.hpp>
 #include <algorithm/mergesort.hpp>
+#include <algorithm/binarysearch.hpp>
 
 template <class ForwardIterator>
 void print(ForwardIterator first, ForwardIterator last)
@@ -23,7 +25,12 @@ int main()
   std::vector<int> vector2 = { 10, 50, 20, 25, 70, 80, 40, 30, 10, 100, 10 };
   algorithm::mergesort(std::begin(vector2), std::end(vector2));
   print(std::begin(vector2), std::end(vector2));
-      
+
+  std::vector<int> vector3 = { 10, 20, 20, 50, 60, 80, 90, 90, 90, 100 };
+  assert(algorithm::binarysearch(std::begin(vector3), std::end(vector3), 40)
+         == std::end(vector3));
+  assert(algorithm::binarysearch(std::begin(vector3), std::end(vector3), 50)
+         == std::begin(vector3) + 3);
   return 0;
 }
     
