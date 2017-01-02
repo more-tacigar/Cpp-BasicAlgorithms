eba9545e240a81307c5bc800bf36e573cb0bfc31
tacigar <ig.lhound@gmail.com>
2017-01-02 17:01:46 +0900

    Update test.cpp for mergesort

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include <list>
 #include <vector>
 #include <algorithm/quicksort.hpp>
+#include <algorithm/mergesort.hpp>
 
 template <class ForwardIterator>
 void print(ForwardIterator first, ForwardIterator last)
@@ -18,6 +19,10 @@ int main()
   std::vector<int> vector1 = { 10, 50, 20, 20, 70, 80, 40, 30, 10, 100, 10 };
   algorithm::quicksort(std::begin(vector1), std::end(vector1));
   print(std::begin(vector1), std::end(vector1));
+
+  std::vector<int> vector2 = { 10, 50, 20, 25, 70, 80, 40, 30, 10, 100, 10 };
+  algorithm::mergesort(std::begin(vector2), std::end(vector2));
+  print(std::begin(vector2), std::end(vector2));
       
   return 0;
 }
